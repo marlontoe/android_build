5dec4660f0c9a5c8f5a2e9157a02cebe5b34c43c
nuclearmistake <nuclearmistake@gmail.com>
2014-11-16 15:16:11 -0500

    Use system ccache if present
    Revert "Use cm's prebuilt ccache, which is a higher version than most distro's ccache package."
    
    This reverts commit d19f5423917d00990af2123d37ec48595d052096.
    
    Conflicts:
    
    	core/combo/select.mk
    	core/tasks/kernel.mk
    
    Change-Id: Ia3bdeb6b58d423d27739a3c853d6d5f498571ed0

--- core/tasks/kernel.mk
+++ core/tasks/kernel.mk
@@ -176,22 +176,17 @@ KERNEL_TOOLCHAIN_PATH := $(KERNEL_TOOLCHAIN)/$(KERNEL_TOOLCHAIN_PREFIX)
 endif
 endif
 
-ifneq ($(USE_CCACHE),)
-    ccache := $(ANDROID_BUILD_TOP)/prebuilts/misc/$(HOST_PREBUILT_TAG)/ccache/ccache
-    # Check that the executable is here.
-    ccache := $(strip $(wildcard $(ccache)))
+ifeq ($(TARGET_ARCH),arm)
+    ifneq ($(USE_CCACHE),)
+      ccache := $(shell which ccache)
+      ifeq ($(ccache),)
+        ccache := $(ANDROID_BUILD_TOP)/prebuilts/misc/$(HOST_PREBUILT_TAG)/ccache/ccache
+      endif
+      ccache := $(strip $(wildcard $(ccache)))
+    endif
+    KERNEL_CROSS_COMPILE := CROSS_COMPILE="$(ccache) $(KERNEL_TOOLCHAIN_PATH)"
+    ccache =
 endif
-
-ifneq ($(TARGET_GCC_VERSION_ARM),)
-  ifeq ($(HOST_OS),darwin)
-    KERNEL_CROSS_COMPILE := CROSS_COMPILE="$(ccache) $(ANDROID_BUILD_TOP)/prebuilts/gcc/darwin-x86/arm/arm-eabi-$(TARGET_GCC_VERSION_ARM)/bin/arm-eabi-"
-  else
-    KERNEL_CROSS_COMPILE := CROSS_COMPILE="$(ccache) $(ANDROID_BUILD_TOP)/prebuilts/gcc/linux-x86/arm/arm-eabi-$(TARGET_GCC_VERSION_ARM)/bin/arm-eabi-"
-  endif
-else
-  KERNEL_CROSS_COMPILE := CROSS_COMPILE="$(ccache) $(KERNEL_TOOLCHAIN_PATH)"
-endif
-ccache =
 
 define mv-modules
     mdpath=`find $(KERNEL_MODULES_OUT) -type f -name modules.order`;\
