88a42d6f3949b946cf2b640132c6cd5d70f912e3
marlontoe <marlontoe@users.noreply.github.com>
2016-11-13 15:21:52 +0100

    use system ccache

--- core/ccache.mk
+++ core/ccache.mk
@@ -44,7 +44,7 @@ ifneq ($(USE_CCACHE),)
   ifeq ($(HOST_OS)-$(BUILD_OS),windows-linux)
     CCACHE_HOST_TAG := linux-$(HOST_PREBUILT_ARCH)
   endif
-  ccache := prebuilts/misc/$(CCACHE_HOST_TAG)/ccache/ccache
+  ccache := /usr/bin/ccache
   # Check that the executable is here.
   ccache := $(strip $(wildcard $(ccache)))
   ifdef ccache
